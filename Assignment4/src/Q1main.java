85debbb2d83ba5cbfe993f57be281eb37e21b449
Ankush sunil sonawane <ankushsonawane60@gmail.com>
2023-10-19 23:24:19 +0530

    Assignment4

---
+++ Assignment4/src/Q1main.java
@@ -0,0 +1,12 @@
+import in.date.Student;
+
+public class Q1main {
+
+	public static void main(String[] args) {
+		Student s1 = new Student(11,"asd",23,10,2023);
+		System.out.println(s1);
+	}
+	
+}
+
+
